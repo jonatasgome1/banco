9a6ab47afc5c9560d72931c66b3819b9574aca6f
jonatasgomes <jonatasg503@gmail.com>
2023-09-15 10:58:23 -0300

    Meu banco

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banco/>
   </div>
 </template>
 
 <script>
+import Banco from'./components/Banco.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Banco
   }
 }
 </script>
---
+++ src/components/Banco.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Meu Banco</h1>
+    <p :style="{ color: saldo >= 0 ? 'green' : 'red' }">Saldo: R$ {{ saldo.toFixed(2) }}</p>
+    <button @click="depositar">Depositar</button>
+    <button @click="sacar">Sacar</button>
+  </div>
+</template>  
+<script>
+export default {
+  name: `Banco`,
+  data() {
+    return {
+      saldo: 100.0,
+    };
+  },
+  methods: {
+    depositar() {
+      const valor = parseFloat(prompt('Digite o valor do depósito:'));
+      if (!isNaN(valor) && valor > 0) {
+        this.saldo += valor;
+      }
+    },
+    sacar() {
+      const valor = parseFloat(prompt('Digite o valor do saque:'));
+      if (!isNaN(valor) && valor > 0) {
+        if (this.saldo >= valor) {
+          this.saldo -= valor;
+        } else {
+          // Se o saldo não for suficiente, apenas subtrai o valor do saldo
+          // Isso fará com que o saldo fique negativo
+          this.saldo -= valor;
+        }
+      }
+    },
+  },
+};
+</script>
+
+  
